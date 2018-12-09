23c96fd04b1e4a4da566eed0ecb7f9e55b7f2a31
zumrat <zuma.shukurova@gmail.com>
2018-12-09 22:04:54 +0200

    Добавлена возможность продолжить игру при желании

--- Java1Lesson3.java
+++ Java1Lesson3.java
@@ -7,26 +7,32 @@ public class Java1Lesson3 {
         guessNumber();
     }
 
-    static String guessNumber() {
+    static void guessNumber() {
         Random rnd = new Random();
-        int number = rnd.nextInt(10);
-        int count = 0;
-        int player = 0;
-        while (count < 3 && player != number) {
-            System.out.println("Enter your number!");
-            player = in.nextInt();
-            if (player == number)
-                System.out.println("You won!");
-            else if (player < number)
-                System.out.println("Your number is greater");
-                else if (player > number)
-                    System.out.println("Your number is less");
-            count++;
+        String repeat;
+        do {
+            repeat = "0";
+            int number = rnd.nextInt(10);
+            int count = 0;
+            int player = 0;
+            while (count < 3 && player != number) {
+                    System.out.println("Enter your number!");
+                    player = in.nextInt();
+                    if (player == number)
+                        System.out.println("You won!");
+                    else if (player < number)
+                        System.out.println("Your number is greater");
+                    else if (player > number)
+                        System.out.println("Your number is less");
+                    count++;
+
+            }
+            if (count == 3 && player!=number)
+                System.out.println("You lost!");
+            System.out.print("Repeat the game?\n[1 - yes / 0 - no]: ");
+            repeat = in.next();
+        } while(repeat.equals("1"));
         }
-        if (count==3 && player!=number) System.out.println("You lose!");
-        String L;
-return L = "You lose!";
-    }
 }
 
 
