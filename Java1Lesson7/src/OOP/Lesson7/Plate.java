ee2bf6e8c4adf92bbccfb3c045454083f54c8cd0
zumrat <zuma.shukurova@gmail.com>
2018-12-24 12:36:28 +0200

    Расширена задача про котов

---
+++ Java1Lesson7/src/OOP/Lesson7/Plate.java
@@ -0,0 +1,28 @@
+package OOP.Lesson7;
+
+public class Plate {
+    private int amountOfFood;
+
+    Plate(int amountOfFood) {
+        this.amountOfFood = amountOfFood;
+    }
+
+    @Override
+    public String toString() {
+        return "PLATE: " + amountOfFood;
+    }
+
+    void decreaseFood(int appetite) {
+        if (amountOfFood >= appetite) {
+            amountOfFood -= appetite;
+        }
+    }
+
+    int getAmountOfFood() {
+        return amountOfFood;
+    }
+
+    void increaseFood(int amount) {
+        amountOfFood += amount;
+    }
+}
